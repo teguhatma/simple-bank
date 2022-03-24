2f14631d7c82b7179b268f167b5ea09bf537a528
Teguh <teguh.yudha@finaccel.co>
2022-03-24 13:36:37 +0700

    #11

---
+++ api/server.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/teguhatma/simple-bank/db/sqlc"
+)
+
+type Server struct {
+	store  *db.SQLStore
+	router *gin.Engine
+}
+
+func NewServer(store *db.SQLStore) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{
+		"error": err.Error(),
+	}
+}
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/teguhatma/simple-bank/api"
+	db "github.com/teguhatma/simple-bank/db/sqlc"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	if err := server.Start(serverAddress); err != nil {
+		log.Fatal("cannot start server", err)
+	}
+}
